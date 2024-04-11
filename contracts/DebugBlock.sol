eb070dbc51e63c475b73e96c5f373ce6f75732f1
Darren Kelly <darren.kelly@vechain.org>
2024-04-11 11:42:17 +0100

    update test

--- contracts/DebugBlock.sol
+++ contracts/DebugBlock.sol
@@ -4,6 +4,13 @@ pragma solidity ^0.8.18;
 contract DebugBlock {
     event BlockEmitted(uint256 blockNumber);
 
+    mapping(uint256 => string) public blockData1;
+    mapping(uint256 => string) public blockData2;
+    mapping(uint256 => string) public blockData3;
+    mapping(uint256 => string) public blockData4;
+    mapping(uint256 => string) public blockData5;
+    mapping(uint256 => string) public blockData6;
+
     uint256 private nextBlock;
 
     function setNextBlock() public {
@@ -13,6 +20,13 @@ contract DebugBlock {
             // extra storage op here
             nextBlock = block.number;
             emit BlockEmitted(nextBlock);
+
+            blockData1[nextBlock] = "data1";
+            blockData2[nextBlock] = "data2";
+            blockData3[nextBlock] = "data3";
+            blockData4[nextBlock] = "data4";
+            blockData5[nextBlock] = "data5";
+            blockData6[nextBlock] = "data6";
         }
     }
 }
--- scripts/debug.ts
+++ scripts/debug.ts
@@ -1,5 +1,5 @@
 import { HttpClient, ThorClient } from "@vechain/sdk-network";
-import { HDNode } from "@vechain/sdk-core";
+import { HDNode, Transaction } from "@vechain/sdk-core";
 import { DebugBlock__factory as ContractFactory } from "../typechain-types";
 import axios from "axios";
 
@@ -24,21 +24,23 @@ const start = async () => {
     const tx1 = await contract.transact.setNextBlock();
     const receipt1 = await tx1.wait();
     console.log(`TX1 block: ${receipt1?.meta.blockNumber}, Gas used: ${receipt1?.gasUsed}`);
+    
+    const clauses = [{
+        to: contract.address,
+        data: contractInterface.encodeFunctionData("setNextBlock"),
+        value: "0x0",
+    }]
 
-    const estimation = await axios.post(`${baseUrl}/accounts/*?revision=${receipt1?.meta.blockID}`, {
-        clauses: [{
-            to: contract.address,
-            data: contractInterface.encodeFunctionData("setNextBlock"),
-            value: "0x0",
-        }]
-    })
+    const simulated = await thorClient.transactions.simulateTransaction(clauses, { revision: receipt1?.meta.blockID })
+    const estimatedGas = await thorClient.gas.estimateGas(clauses);
 
-    const emittedBlock = estimation.data[0].events[0].data;
-    console.log(`Estimated block: ${BigInt(emittedBlock).toString()}, Gas used: ${estimation.data[0].gasUsed}`);
+    // @ts-ignore
+    console.log(`Estimated block: ${BigInt(simulated[0].events[0].data).toString()}, Gas used: ${estimatedGas.totalGas}`);
 
     const { wait } = await contract.transact.setNextBlock();
     const receipt = await wait();
 
+    console.log(receipt?.reverted ? "TX2 reverted" : "TX2 success");
     console.log(`TX2 block: ${receipt?.meta.blockNumber}, Gas used: ${receipt?.gasUsed}`);
 }
 
